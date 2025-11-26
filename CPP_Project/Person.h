600f0f3003208daa75b91e8e4dd1e434b54eac69
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-27 04:36:52 +0900

    step1

---
+++ CPP_Project/Person.h
@@ -0,0 +1,26 @@
+﻿#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using namespace std;
+
+class Person {
+protected:
+	string name;
+	int id;
+public:
+	Person(string n, int i) :name(n), id(i) {}
+	virtual ~Person() {}
+
+	virtual void display() = 0;
+	virtual string getType() = 0;
+
+	string getName() {
+		return name;
+	}
+	int getId() {
+		return id;
+	}
+};
+#endif // !PERSON_H
+
--- CPP_Project/main.cpp
+++ CPP_Project/main.cpp
@@ -1,41 +1,7 @@
 ﻿#include <iostream>
-#include "character.h"
+#include "Person.h"
 using namespace std;
 
-class test1character : public character {
-public:
-	test1character(string name, int level) :character(name, level) {}
-
-	void showstat() override {
-		cout << "--------능력치----------------" << endl;
-		cout << "이름: " << name << endl;
-		cout << "레벨: " << level << endl;
-		cout << "체력: " << hp << "|" << maxhp << endl;
-		cout << "공격력: " << attack << endl;
-		cout << "방어력: " << defense << endl;
-		cout << "경험치: " << exp << "|" << maxexp << endl;
-		cout << "------------------------------" << endl;
-	}
-
-	int attacktarget() override {
-		cout << name << "의 공격" << endl;
-		return attack;
-	}
-};
-
 int main() {
-	cout << "---클래스 테스트---" << endl;
-
-	test1character hero("생존자", 1);
-	test1character enomy("구울", 1);
-
-	hero.showstat();
-	enomy.showstat();
-
-	cout << "싸움" << endl;
-
-	int damage = hero.attacktarget();
-	enomy.takedamage(damage);
-
-
+	cout << "Person 클래스 생성 완료" << endl;
 }
